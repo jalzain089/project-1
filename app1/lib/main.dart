b9bede776244e64749447c91526651e65cd364d1
Joud <jalzain.0@gmail.com>
2024-08-08 00:16:57 +0300

    test

---
+++ app1/lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'user.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 60, 58, 183)),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+bool isSecured = true;
+
+Map<String, User> users = {
+  "": User(email: 'joudkhalid@gmail.com', password: '1234'),
+  "jayjay@gmail.com": User(email: 'jayjay@gmail.com', password: 'j123'),
+};
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 60, 58, 183),
+        centerTitle: true,
+        title: Text(
+          "Discord",
+          style: TextStyle(fontSize: 25, color: Colors.white),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Image.asset(
+              'assets/asset.png',
+              width: 350,
+              height: 350,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: emailController,
+                    decoration: InputDecoration(
+                      label: Text('Enter Email'),
+                      prefixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: isSecured,
+                    decoration: InputDecoration(
+                      label: Text('Enter Password'),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            isSecured = !isSecured;
+                          });
+                        },
+                        icon: Icon(Icons.remove_red_eye),
+                      ),
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                  InkWell(
+                    onTap: () {
+                      User user = users[emailController.text]!;
+
+                      if (user.email == emailController.text &&
+                          user.password == passwordController.text) {
+                        print('logged');
+                      } else {
+                        print('email/password is wrong');
+                      }
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: Color.fromARGB(255, 60, 58, 183),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Login',
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Icon(
+                            Icons.login,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
